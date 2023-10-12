7782033ff05a78ad8153b9d6073f3766d66a58bb
Vini1604 <vinicius.oliveira1998@hotmail.com>
2023-10-12 13:07:30 -0300

    PROC Inserir user alterado

--- pInserirUsuario.sql
+++ pInserirUsuario.sql
@@ -1,5 +1,5 @@
 DELIMITER //
-CREATE PROCEDURE pInserirUsuario(IN nomeUsuario VARCHAR(255), IN cpfUsuario VARCHAR(255), IN numeroUsuario VARCHAR(255), IN dataNascimentoUsuario DATE, IN senhaUsuario VARCHAR(1000))
+CREATE PROCEDURE pInserirUsuario(IN nomeUsuario VARCHAR(255), IN cpfUsuario VARCHAR(255), IN numeroUsuario VARCHAR(255), IN emailUsuario VARCHAR(100), IN dataNascimentoUsuario DATE, IN senhaUsuario VARCHAR(1000))
 BEGIN
 	DECLARE cpfExistentes INT;
     SELECT COUNT(*) INTO cpfExistentes FROM usuario WHERE cpf = cpfUsuario;
@@ -8,7 +8,7 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Já existe um usuário com esse CPF';
     ELSE
         -- Caso contrário, insere o registro na tabela
-        INSERT INTO usuario(nome, cpf, numero, data_nascimento, data_inicio, ativo, senha) VALUES (nomeUsuario, cpfUsuario, numeroUsuario, dataNascimentoUsuario, NOW(), 1, senhaUsuario);
+        INSERT INTO usuario(nome, cpf, numero, data_nascimento, data_inicio, ativo, senha, email) VALUES (nomeUsuario, cpfUsuario, numeroUsuario, dataNascimentoUsuario, NOW(), 1, senhaUsuario, emailUsuario);
     END IF;
 END //
 DELIMITER ;
